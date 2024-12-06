7f5135d45aba80f278106a3c26e487c8b2804afe
Arata M Katayama <aratamkatayama@Aratas-Laptop.local>
2024-12-05 20:10:53 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import ImgCarousel from './components/carousel/ImgCarousel';
-import Destinations from './components/destinations/Destinations'
-import Footer from './components/footer/Footer';
-import Hero from './components/hero/Hero'
-import Navbar from './components/navbar/Navbar'
-import Search from './components/search/Search';
-import Selects from './components/selects/Selects';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Destinations />
-      <Search />
-      <Selects />
-      <ImgCarousel />
-      <Footer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
